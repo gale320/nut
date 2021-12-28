1ccaccf20ce4d0b269340f52651de12c616fe505
gale320 <gale320@gmail.com>
2021-12-28 22:06:04 +0800

    add some code

---
+++ include/nutinc.h
@@ -0,0 +1,18 @@
+#ifndef __NUTINC_H__
+#define __NUTINC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif    
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
---
+++ src/utils/nututils.h
@@ -0,0 +1,11 @@
+#ifndef __NUTUTILS_H__
+#define __NUTUTILS_H__
+
+
+
+
+
+
+
+#endif
+
